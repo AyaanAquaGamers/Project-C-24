60e2eb408beab2993a5df70ffc84585edb8f010a
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-05-15 17:20:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,53 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body=Matter.Body
+const Constraint = Matter.Constraint;
 
-var engine, world;
-var stone,iron,rubber,hammer;
-var sand1,sand2,sand3,sand4,sand5,sand6
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
 
-
-function setup(){
-    var canvas = createCanvas(1200,600);
-    engine = Engine.create();
-    world = engine.world;
-
-    plane = new Plane(600,height,1200,20)
-
-    stone=new Stone(700,320,100,100)
-    iron=new Iron(300,350)
-    rubber=new Rubber(900,450,70)
-    hammer = new Hammer(10,100);
-
-    sand1=new Sand(505,450,10)
-    sand2=new Sand(510,450,10)
-    sand3=new Sand(515,450,10)
-    sand4=new Sand(520,450,10)
-    sand5=new Sand(525,450,10)
-    sand6=new Sand(530,450,10)
-    sand7=new Sand(535,450,10)
-    sand8=new Sand(540,450,10)
-    sand9=new Sand(545,450,10)
-    sand10=new Sand(550,450,10)
+function setup() {
+  createCanvas(900, 400);
   
+  engine = Engine.create();
+  world = engine.world;
 
+  ground = new Ground(200, 400, 600, 5);
+
+  hero = new Hero(400,3,100,100);
+  rope = new Rope(hero.body, { x: 100, y: 465 });
+  monster = new Monster(380,200,100,100);
+
+  box1 = new Box(280,350, 30, 40);
+  box2 = new Box(310, 350, 30, 40);
+  box3 = new Box(340, 350, 30, 40);
+  box4 = new Box(370, 350, 30, 40);
+
+  Engine.run(engine)
 
 }
 
-function draw(){
-    background("lightBlue");
-    Engine.update(engine);
+function draw() {
+  rectMode(CENTER)
+  background(bg);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
 
-    stone.display()
-    plane.display()
-    iron.display()
-    hammer.display();
+  hero.display();
+  rope.display();
+  monster.display();
 
-    sand1.display()
-    sand2.display()
-    sand3.display()
-    sand4.display()
-    sand5.display()
-    sand6.display()
-    sand7.display() 
-    sand8.display() 
-    sand9.display() 
-    sand10.display() 
-     
- 
+}
+function mousePressed(){
+  Matter.Body.setPosition(hero.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+  attach.fly()
 }
